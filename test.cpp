de106bde843f8f5bf4b981a19fc59f1d41c346b4
bobdidley <james.austin@student.csulb.edu>
2021-01-17 15:35:06 -0800

    file test

---
+++ test.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main() {
+    // TEST
+    /* File test:
+     * Reads from file
+     */
+    std::ifstream wordList("/usr/share/dict/words");
+    if(wordList.is_open())
+        std::cout << "Open file" << std::endl;
+    std::string word;
+    for(int i = 0; i < 50; ++i) {
+        // extracts then discards
+        wordList.ignore();
+
+        // gives char value of first word
+        // std::cout << wordList.peek() << std::endl;
+    }
+    wordList >> word;
+    std::cout << word;
+
+    // opens website using word extracted from file
+    system(("open https://www.merriam-webster.com/" + word).c_str());
+
+    return 0;
+}
