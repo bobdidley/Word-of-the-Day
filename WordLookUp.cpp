d230eff6ed1dcece3048d499b51ffef8c58881cf
bobdidley <james.austin@student.csulb.edu>
2021-01-17 19:33:31 -0800

    isNewWord() code

--- WordLookUp.cpp
+++ WordLookUp.cpp
@@ -25,7 +25,42 @@ WordLookUp :: ~WordLookUp() {
 }
 
 bool WordLookUp :: isNewWord(std::string newWord) {
+    bool isNew = true;
+    std::ifstream wordHistory("wordHistory.txt");
 
+    if(!wordHistory.is_open()) { throw std::string("No file exists..."); }
+
+    wordHistory.seekg(-1, std::ios_base::end);
+
+    std::string latestDetails;
+    int indexDateBegin = 0, indexDateEnd = 10;
+    int indexWordBegin = 12;
+
+    std::getline(wordHistory, latestDetails);
+    std::string latestDate = latestDetails.substr(indexDateBegin, indexDateEnd);
+    std::string latestWord = latestDetails.substr(indexWordBegin);
+    std::string todayDate = "2021-01-17"; // NOTE: <ctime>? need to format date
+
+    if(latestDate == todayDate) {
+        isNew = false;
+    }
+    else if (latestWord == newWord) {
+        isNew = false;
+    }
+    else {
+        wordHistory.seekg(0, std::ios_base::beg); // goes to beginning of file
+        std::string details;
+        while(!wordHistory.eof()) {
+            std::getline(wordHistory, details);
+            if(details.substr(indexWordBegin) == newWord) {
+                isNew = false;
+                break; // exit loop if there has been a match
+            }
+            wordHistory.ignore(); // only necessary if getline() does not go to next line after
+        }
+    }
+
+    return isNew;
 }
 
 void WordLookUp :: searchDetails(std::string w) {
