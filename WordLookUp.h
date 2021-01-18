52bc911ac47f9a55527b07d1f742d741efaeec67
bobdidley <james.austin@student.csulb.edu>
2021-01-17 19:47:48 -0800

    some header documentation

--- WordLookUp.h
+++ WordLookUp.h
@@ -31,9 +31,25 @@ class WordLookUp {
         void setTag(std::string);
 
     public:
+        /**
+         * @brief Construct a new Word Look Up object
+         *        Begins to search for a new word and it's details
+         * 
+         */
         WordLookUp();
+        /**
+         * @brief Destroy the Word Look Up object
+         * 
+         */
         ~WordLookUp();
 
+        /**
+         * @brief Checks if the parameter is in the word history, true if it isn't, false if it is
+         *        Will determine if the word passed through is a new word
+         * 
+         * @return true : default value, true if the word passed in is not in the word history
+         * @return false : if the word passed in is in the word history
+         */
         bool isNewWord(std::string);
         void searchDetails(std::string);
 
