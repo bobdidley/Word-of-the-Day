c1c93955e9e93534c925c732c5eccfc0a62a5624
bobdidley <james.austin@student.csulb.edu>
2021-05-29 15:13:25 -0700

    discontinuing this project

--- main.cpp
+++ main.cpp
@@ -8,3 +8,39 @@
  * @copyright Copyright (c) 2021
  * 
  */
+
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <curl/curl.h>
+
+int main() {
+    srand(time(0));
+    std::ifstream wordList("/usr/share/dict/words");
+    if(wordList.is_open())
+        std::cout << "Open File" << std::endl;
+        char choice = 'y';
+        std::string word;
+        while(choice == 'y') {
+            for(int i = 0; i < unsigned(rand()); ++i) {
+                // extracts then discards
+                wordList.ignore();
+
+                // gives char value of first word
+                // std::cout << wordList.peek() << std::endl;
+            }
+            wordList >> word;
+            std::cout << word << std::endl;
+
+            // opens website using word extracted from file
+            system(("open https://www.merriam-webster.com/dictionary/" + word).c_str());
+
+            std::cout << "Another word? (y/n) ";
+            std::cin >> choice;
+            std::cout << "\n";
+        }
+        wordList.close();
+
+    return 0;
+}
